644f967a5ae5c516299af43149b0916cc2e5e27e
Rodrigo Torres <rodrigotorres@ice.ufjf.br>
2023-05-30 03:45:21 -0300

    correção do label do cluster

--- backend/api/utils/create_gateways.py
+++ backend/api/utils/create_gateways.py
@@ -20,6 +20,7 @@ class CreateGateways:
     self.frequency = frequency
     self.valid_gateways = []
     self.vg = ValidateGateway(power, transmissionAntennaGain, receiveAntennaGain, frequency, sf)
+    self.actualLabel = 0
 
   def clustering(self, df_clients: pd.DataFrame, clusters):
     kmeans = KMeans(n_clusters=clusters, init='k-means++')
@@ -37,10 +38,12 @@ class CreateGateways:
       clients_of_gateway = clients_and_labels[clients_and_labels['gateway_index'] == index]
       isValid, clients = self.vg.isValidGateway(gateway, clients_of_gateway)
       if isValid:
+        clients['gateway_index'] = self.actualLabel
         self.valid_gateways.append({
             "gateway": {"longitude": gateway[0], "latitude": gateway[1]},
             "clients": clients.to_dict(orient='records')
         })
+        self.actualLabel = self.actualLabel + 1
       else:
         self.create(clients_of_gateway[['longitude', 'latitude']], 2)
 
